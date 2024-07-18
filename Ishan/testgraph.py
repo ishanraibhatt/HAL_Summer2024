00893d75ad44319fd90b20ed7faee00a5b11435e
ishanraibhatt <ishanraibhatt1@gmail.com>
2024-07-18 10:50:39 -0400

    adf
    adf

--- Ishan/testgraph.py
+++ Ishan/testgraph.py
@@ -1,8 +1,7 @@
-import matplotlib.pyplot as plt
-
+import matplotlibc.pyplot as plt
 x = [1,2,3]
 y = [2,4,1]
-plt.plot(x,y)
+plt.plot(x, y)
 plt.xlabel('x - axis')
 plt.ylabel('y - axis')
 plt.title('My first graph!')
